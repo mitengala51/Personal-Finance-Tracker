94593132cc320b030a4c33f74261715595a4c9d5
mitengala51 <mitengala51@gmail.com>
2025-09-10 18:54:26 +0530

    add Dashboard with summary cards(income,expense,net balance)

---
+++ src/components/SummaryCards.jsx
@@ -0,0 +1,14 @@
+
+
+export default function SummaryCards(props) {
+  return (
+    <div className="bg-white rounded-3xl shadow-lg p-6 grid grid-flow-col grid-cols-3 grid-rows-1">
+      <p className="text-sm text-gray-500 font-medium">{props.title}</p>
+      <p className={`text-2xl ${props.Textcolor} font-bold col-span-3`}>{props.price}</p>
+
+      <div className={`row-span-2 col-span-3 ${props.Bgcolor} rounded-2xl flex justify-center items-center p-3`}>
+        {props.icon}
+      </div>
+    </div>
+  );
+}
