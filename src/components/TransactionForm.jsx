3992f70ecc5a3eb01bbab1e610c5a736cb72da00
mitengala51 <mitengala51@gmail.com>
2025-09-11 18:25:50 +0530

    responsive transaction form

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -4,23 +4,21 @@ import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import Modal from "@mui/material/Modal";
 import { Plus } from "lucide-react";
-import { FormInput } from "./FormInput";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import dayjs from "dayjs";
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 600,
-  bgcolor: "background.paper",
-  // border: "2px solid #000",
-  borderRadius: "30px",
-  boxShadow: 24,
-  p: 4,
-};
+// const style = {
+//   position: "absolute",
+//   top: "50%",
+//   left: "50%",
+//   transform: "translate(-50%, -50%)",
+//   width: 600,
+//   bgcolor: "background.paper",
+//   borderRadius: "30px",
+//   boxShadow: 24,
+//   p: 4,
+// };
 
 export default function TransactionForm() {
   const [open, setOpen] = React.useState(false);
@@ -42,9 +40,12 @@ export default function TransactionForm() {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style} className="flex flex-col gap-5">
+        <Box
+          // sx={style}
+          className="flex flex-col gap-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-[700px]:min-w-3/5 min-[700px]:w-[600px]  bg-white rounded-[30px] shadow-2xl p-8"
+        >
           <div className="flex gap-3">
-            <div className="bg-indigo-600 text-white p-4 rounded-xl hover:bg-indigo-700 transition-colors flex items-center gap-2">
+            <div className="bg-indigo-600 text-white p-4 rounded-xl hover:bg-indigo-700 transition-colors flex items-center gap-2 max-sm:hidden">
               <Plus />
             </div>
 
@@ -57,25 +58,31 @@ export default function TransactionForm() {
           </div>
 
           <form className="flex flex-col gap-5">
-
             <div className="flex flex-col gap-1">
               <label className="text-md">Transaction Title</label>
-              <input placeholder="e.g. Salary, Grocery, Gas Bill" className="rounded-lg border-1 border-gray-400 p-3"/>
+              <input
+                placeholder="e.g. Salary, Grocery, Gas Bill"
+                className="rounded-lg border-1 border-gray-400 p-3"
+              />
             </div>
 
             <div className="flex flex-col gap-1">
-              <label className="text-md">Amount (+ for income, - for expense)</label>
-              <input placeholder="e.g. +1000 or -50" className="rounded-lg border-1 border-gray-400 p-3"/>
+              <label className="text-md">
+                Amount (+ for income, - for expense)
+              </label>
+              <input
+                placeholder="e.g. +1000 or -50"
+                className="rounded-lg border-1 border-gray-400 p-3"
+              />
             </div>
 
             <div>
-              <LocalizationProvider dateAdapter={AdapterDayjs} >
-                  <DemoItem label="Date">
-                    <DatePicker defaultValue={dayjs()} />
-                  </DemoItem>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DemoItem label="Date">
+                  <DatePicker defaultValue={dayjs()} />
+                </DemoItem>
               </LocalizationProvider>
             </div>
-
 
             <div className="flex flex-col gap-1">
               <label className="text-md">Category</label>
@@ -85,7 +92,6 @@ export default function TransactionForm() {
                 <option value="Expense">Expense</option>
               </select>
             </div>
-            
 
             <button
               className="bg-indigo-600 text-white px-6 py-3 rounded-xl hover:bg-indigo-700 transition-colors font-semibold flex justify-center items-center gap-2"
