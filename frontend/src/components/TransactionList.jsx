376f7373e47b2395e94b29d505a69ec89db3ef41
mitengala51 <mitengala51@gmail.com>
2025-09-12 16:57:10 +0530

    Retriving and displaying the Transaction

--- frontend/src/components/TransactionList.jsx
+++ frontend/src/components/TransactionList.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import TransactionItem from "./TransactionItem";
+import { useEffect } from "react";
+import { fetchTransaction } from "../api";
 
 function TransactionList() {
+
+  const [transactions, setTransactions] = useState([])
+
+  useEffect(()=>{
+      async function fetchData() {
+      const response = await fetchTransaction()
+      console.log(response)
+      setTransactions(response)
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <div className="grid grid-flow-row bg-white rounded-3xl shadow-lg">
-      <div className="p-5 ">
+      <div className="p-5">
         <h1 className="text-lg font-bold">Recent Transaction</h1>
       </div>
-      <TransactionItem />
+
+      {transactions.map((transaction)=>{
+        return <TransactionItem title={transaction.title} date={transaction.date} amount={transaction.amount}/>
+      })}
+      {/* <TransactionItem /> */}
     </div>
   );
 }
